59f85bf20b789b26b43676d3792959b7a764147c
Adrian <onicaadrian2@gmail.com>
2025-09-13 10:28:07 +0300

    Add Models (Appointment, User)

---
+++ Models/Appointment.cs
@@ -0,0 +1,6 @@
+﻿public class Appointment
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public DateTime Date { get; set; }
+}
---
+++ Models/User.cs
@@ -0,0 +1,6 @@
+﻿public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public string Role { get; set; } = "User";
+}
